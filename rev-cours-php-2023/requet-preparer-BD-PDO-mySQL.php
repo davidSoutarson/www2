8ab91fa14278b4d5ccdd952e2afb9481f76c979f
david soutarson <triplaxs@gmail.com>
2022-09-08 21:47:20 +0200

    revtion coure php + exemple de conetion BD mySQL + exemple de Creation Table + exemple insertion de donner + requet prepare pour incertion

--- rev-cours-php-2023/requet-preparer-BD-PDO-mySQL.php
+++ rev-cours-php-2023/requet-preparer-BD-PDO-mySQL.php
@@ -1,20 +1,22 @@
 <?php
-echo "insertion de doner avec PDO MySQL a patire d'sripte php.<br>";
+echo "<p>Les requete preparer: une metode conventionnele pour
+      <br>l'insertion de doner avec PDO MySQL a patire d'sripte php.</p>";
 
 $serveur = "localhost";
 $login ="root";
 $pass = "";
 
+$nom ="Meliodas";
+$prenom ="Dragon-de-la-colaire";
+$email ="seven.deadly.sins@live.fr"; /*Les valeur stoker dans les varible
+$nom $prenom $email sont normalement des valeur isue d'un fomulaire et les teste
+de validation de formulaire*/
 try {
   $connexion = new PDO("mysql:hots=$serveur;dbname=test2",$login,$pass);/* Conextion a MySQL (dbname=teste2 modifiable en fontion:
                                                                           * Du non de la Base bdname=non_de_la_base )*/
   $connexion -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-  $insertion ="INSERT INTO Visiteurs(nom,prenom,email)
-                          VALUES('Soutarson','David','triplaxs@gmail.com')";
-
-  $connexion ->exec($insertion);
-  echo "insertion reusie";
+  echo "teste Conection a la base de donner reusie";
 }
 catch (PDOException $e) {
   echo "Echec insertion :" . $e ->getMessage();
--- rev-cours-php-2023/sta-inser-mutiple-BD-PDO-mySQL.php
+++ rev-cours-php-2023/sta-inser-mutiple-BD-PDO-mySQL.php
@@ -5,13 +5,20 @@ $serveur = "localhost";
 $login ="root";
 $pass = "";
 
+$nom ="factice";                 // Les valeur stoker dans les varible
+$prenom ="user";                  // $nom $prenom $email sont normalement des
+$email ="factice.user@live.fr";   // valeur isue d'un fomulaire et les teste
+                                 // de validation de formulaire
+
 try {
   $connexion = new PDO("mysql:hots=$serveur;dbname=test2",$login,$pass);/* Conextion a MySQL (dbname=teste2 modifiable en fontion:
                                                                           * Du non de la Base bdname=non_de_la_base )*/
   $connexion -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
   $insertion ="INSERT INTO Visiteurs(nom,prenom,email)
-                          VALUES('Soutarson','David','triplaxs@gmail.com')";
+                          VALUES('Durant','Victor','victor.durant@gmail.com'),
+                          ('Dupon','inspecteur','tintin@gmail.com'),
+                          ('$nom','$prenom','$email')";
 
   $connexion ->exec($insertion);
   echo "insertion reusie";
